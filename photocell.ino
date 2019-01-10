3c2a29be8349b2bc10339ccd8b057aabc7c1f448
Centro Ricerca e Sviluppo Spazio Chirale <46569915+spaziochirale@users.noreply.github.com>
2019-01-10 21:21:55 +0100

    Versione iniziale

---
+++ photocell.ino
@@ -0,0 +1,32 @@
+/*******************************************
+ * CORSO ELETTRONICA WEARABLE CON ARDUINO 
+ * 
+ * Author: Rosita Esposito
+ * Ver: 1.0
+ * Title: Esempio di gestione del fotoresistore
+ * 
+ * Circuit: Board Adafruit Flora con sensore di suono a microfono
+ * 
+ */
+
+int photocellPin=A0;
+int photocellReading;
+int pinled = 13;
+
+void setup(void) {
+    Serial.begin(9600);
+    pinMode(13, OUTPUT);
+    }
+    
+void loop(void) {
+  photocellReading = analogRead(photocellPin);  
+  if (photocellReading < 400) { 
+    digitalWrite (pinled, HIGH);
+    } 
+  else {
+   digitalWrite (pinled, LOW);
+  }
+  Serial.print("Analog reading = ");
+  Serial.println(photocellReading); 
+  delay(1000); 
+}
